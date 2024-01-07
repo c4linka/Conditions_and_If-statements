692789677c610b161b14955d0ff802565f093577
Ewa <155647516+c4linka@users.noreply.github.com>
2024-01-07 13:41:43 +0100

    Add files via upload
    practicing Conditions and If statements. 3rd day of learning. Having great fun!:)

---
+++ Conditions and If statements.py
@@ -0,0 +1,10 @@
+a = int(input("Write first numer "))
+b = int(input("Write second numer "))
+
+if (a > b):
+    print("a is bigger then b")
+
+elif (b > a):
+    print("b is bigger then a")
+else:
+    print("a is equal to b")
